c26ab30305d79cb31dcd4ce9ab73785002ecc56b
Abena <priscillaabenaaboagye@gmail.com>
2025-07-10 13:10:06 +0000

    My work

--- exercises_Abena.py
+++ exercises_Abena.py
@@ -1,13 +1,18 @@
-# Q1 
+# Q1 , Q 5
 name = "Priscilla Abena Aboagye"
 print(name)
+print(name[0])
 
-# Q2 
+# Q2 , Q6 , Q7
 sentence ="I hope the cats are fine"
 print(sentence)
 print(len(sentence))
+print(sentence[-1])
+print(sentence[:4])
+print(sentence[1::-2])
+print(sentence[::-1])
 
-# Q3 
+# Q3 , 
 print("Hello" + "World")
 
 # Q4
