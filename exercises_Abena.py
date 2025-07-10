939cf8bc365b3439c24a3dc079c33815328295fb
Abena <priscillaabenaaboagye@gmail.com>
2025-07-10 12:55:39 +0000

    First exercise

---
+++ exercises_Abena.py
@@ -0,0 +1,16 @@
+# Q1 
+name = "Priscilla Abena Aboagye"
+print(name)
+
+# Q2 
+sentence ="I hope the cats are fine"
+print(sentence)
+print(len(sentence))
+
+# Q3 
+print("Hello" + "World")
+
+# Q4
+var = "Cilla"
+mul = var * 5
+print(mul)
